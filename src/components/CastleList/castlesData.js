9e7f7b8e56143f64fa04e4d708447d40b856514d
Sebastian Schulz <45721059+sebastianSchulzDev@users.noreply.github.com>
2019-05-21 12:52:14 +0200

    Update castles information.

--- src/components/CastleList/castlesData.js
+++ src/components/CastleList/castlesData.js
@@ -5,7 +5,7 @@ export const castlesData = [
     img: "./img/pieskowa_skala.jpg",
     name: "Pieskowa Skała",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Zamek Pieskowa Skała – zamek na terenie wsi Sułoszowa, w granicach jednego z trzech jej sołectw, położony w Dolinie Prądnika nieopodal Krakowa, na terenie Ojcowskiego Parku Narodowego."
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const castlesData = [
     img: "./img/malbork.jpg",
     name: "Malbork",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Zamek w Malborku (niem. Ordensburg Marienburg) – zamek w Malborku, na prawym brzegu Nogatu, wzniesiony w kilku etapach od 1280 do poł. XV w. przez zakon krzyżacki. Początkowo konwentualny i siedziba komtura, w latach 1309–1457 siedziba wielkich mistrzów zakonu krzyżackiego i władz Prus Zakonnych, w latach 1457–1772 rezydencja królów Polski, od 1466 siedziba władz Prus Królewskich, od 1568 siedziba Komisji Morskiej, w 1772 zajęty przez administrację Królestwa Prus i zdewastowany w latach 1773–1804; rekonstruowany w latach 1817–1842 i 1882–1944, zniszczony w 1945, ponownie rekonstruowany od 1947; w 1949 wpisany do rejestru zabytków, w 1994 uznany za pomnik historii, w 1997 wpisany na listę światowego dziedzictwa UNESCO; od 1961 siedziba Muzeum Zamkowego w Malborku."
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const castlesData = [
     img: "./img/bielsko_biala.jpg",
     name: "Bielsko-Biała",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Zamek książąt Sułkowskich – najstarsza i największa zabytkowa budowla Bielska-Białej, położona we wschodniej części Starego Miasta. Został wzniesiony w XIV wieku staraniem księcia cieszyńskiego Przemysława I Noszaka i przez kolejne stulecia służył jako jedna z siedzib Piastów cieszyńskich. Jest typem zamku miejskiego od początku włączonego w system fortyfikacji Bielska i będącego zarazem najsilniejszym elementem jego obwarowań. Pełnił także rolę śląskiej warowni granicznej, strzegąc wschodniej granicy Księstwa Cieszyńskiego na rzece Białej – oddzielającej Śląsk od Małopolski."
   },
   {
     id: 4,
@@ -29,6 +29,6 @@ export const castlesData = [
     img: "./img/wawel.jpg",
     name: "Wawel - Kraków",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Zamek Królewski na Wawelu – zamek obronno-rezydencyjny w Krakowie, na Wawelu, o powierzchni 7040 m² z 71 salami wystawowymi. Oddział Zamku Królewskiego na Wawelu – Państwowych Zbiorów Sztuki. Zamek był na przestrzeni wieków wielokrotnie rozbudowywany i odnawiany. Liczne pożary, grabieże i przemarsze obcych wojsk, połączone z niszczeniem rezydencji powodowały, iż obiekt wielokrotnie odbudowywano w nowych stylach architektonicznych oraz remontowano jego szatę zewnętrzną, a także przekształcano i zmieniano wygląd i wyposażenie wnętrz."
   }
 ];
